018d4e4f7e142317c8300a6be6705e9cea978d55
Lucian Musat <amibadesign@gmail.com>
2024-02-11 19:54:13 +0100

    Check in the evening

--- main.py
+++ main.py
@@ -183,7 +183,7 @@ def main():
 
 if __name__ == "__main__":
     logging.info("Starting ohMygGAD!")
-    schedule.every().day.at("08:00", "Europe/Amsterdam").do(main)
+    schedule.every().day.at("20:00", "Europe/Amsterdam").do(main)
     while True:
         schedule.run_pending()
         time.sleep(60)
