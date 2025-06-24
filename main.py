46278be4f69ae1a63eb4c44d8113301e111ce842
Lucian Musat <lucian.musat@hotmail.com>
2025-06-24 19:20:54 +0200

    Update months

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ color_map = {
 }
 
 DUTCH_MONTHS = {
-    'jan': 1, 'feb': 2, 'mrt': 3, 'apr': 4, 'mei': 5, 'jun': 6,
+    'jan': 1, 'feb': 2, 'mrt': 3, 'apr': 4, 'mei': 5, 'jun': 6, 'juni': 6,
     'jul': 7, 'aug': 8, 'sep': 9, 'okt': 10, 'nov': 11, 'dec': 12
 }
 
